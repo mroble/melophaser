7ccdf7b973cae51ecbaaa7f93872616cb7994892
Maureen Roble <moroble@gmail.com>
2017-03-15 11:53:51 -0400

    add physics to melo

--- js/stateMain.js
+++ js/stateMain.js
@@ -6,12 +6,18 @@ var StateMain = {
     },
 
     create: function () {
+        game.physics.startSystem(Phaser.Physics.ARCADE);
         this.robot=game.add.sprite(150,150, "melo");
         this.melo.animations.add("idle", [0,1,2], 9, true); //you can increase to 12 later after you fix the spritesheet ARGH
         this.melo.animations.add("walk", [3,4], 9, true);
         this.melo.animations.add("jump", [5], 9, false);
 
-        this.melo.animations.play("walk");
+        this.melo.animations.play("idle");
+        this.melo.anchor.set(0.5, 0.5);
+        game.physics.arcade.enable(this.melo);
+        this.melo.body.gravity.y=100;
+        this.melo.body.bounce.set(0.25);
+        this.melo.body.collideWorldBound=true;
 
     },
 
